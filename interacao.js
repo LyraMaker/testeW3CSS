eef0146edfb9dc19963b2f2ea25d3ed25793b49f
LyraMaker <gabriel.lyrat@gmail.com>
2020-06-09 22:53:10 -0300

    Add files via upload

---
+++ interacao.js
@@ -0,0 +1,5 @@
+var icon = document.querySelector("#nav-icon");
+
+icon.addEventListener("click",function(event){
+    var menu = document.querySelector("#menu-topo-mobile").classList.toggle("w3-show");
+})
